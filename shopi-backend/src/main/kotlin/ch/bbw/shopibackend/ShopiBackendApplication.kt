c328b6c5233675ef78b4c129c94f8de41141539d
Henry Joerg <henry.joerg@gmail.com>
2023-10-30 08:21:14 +0100

    initial commit

---
+++ shopi-backend/src/main/kotlin/ch/bbw/shopibackend/ShopiBackendApplication.kt
@@ -0,0 +1,11 @@
+package ch.bbw.shopibackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopiBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShopiBackendApplication>(*args)
+}
