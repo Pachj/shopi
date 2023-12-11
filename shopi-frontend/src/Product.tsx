4017fb411789d0bb9a3394ca785efc7265581c05
Henry Joerg <henry.joerg@gmail.com>
2023-12-11 07:44:19 +0100

    WIP

---
+++ shopi-frontend/src/Product.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Product = () => {
+    return (
+        <div>
+            <h2>Product</h2>
+            <img />
+            <p>Price</p>
+        </div>
+    );
+};
+
+export default Product;
